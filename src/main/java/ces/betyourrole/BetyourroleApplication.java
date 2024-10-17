4960640c70d8c9aca9eab5af133b6c8e30b70aab
un-known0 <nam0221@naver.com>
2024-10-17 22:58:03 +0900

    feat: initalization spring

---
+++ src/main/java/ces/betyourrole/BetyourroleApplication.java
@@ -0,0 +1,13 @@
+package ces.betyourrole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetyourroleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetyourroleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ces/betyourrole/BetyourroleApplicationTests.java
@@ -0,0 +1,13 @@
+package ces.betyourrole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetyourroleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
